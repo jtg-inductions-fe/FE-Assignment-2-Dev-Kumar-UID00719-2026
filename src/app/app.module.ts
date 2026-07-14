547202a6948bb90d1119ea7e42bcfb5ad014c6e0
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-07-14 17:09:42 +0530

    [DS_A2_01] : changed folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { GlobalErrorHandlerService } from './services/global-error-handler.service';
+import { GlobalErrorHandlerService } from './core/services/global-error-handler.service';
 
 @NgModule({
     declarations: [AppComponent],
