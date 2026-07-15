5b89ee0b810c6fbf4e0fe9e40428273f0c95adab
Dev Singh <dev.singh@joshtechnologygroup.com>
2026-07-15 12:00:26 +0530

    [DS_A2_01] : implemented custom button component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
-
 import { ErrorComponent } from './errorComponent/error.component';
 
 @NgModule({
     declarations: [ErrorComponent],
     imports: [CommonModule, SharedModule],
+    exports: [ErrorComponent],
 })
 export class FeaturesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+
+import { ButtonComponent } from './components/app-button/app-button.component';
+
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    declarations: [ButtonComponent],
+    imports: [CommonModule, MatButtonModule],
+    exports: [ButtonComponent],
 })
 export class SharedModule {}
